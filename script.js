08341b245cc8eadb6d6d596f9b991e33ee5b8be6
kapilraj7 <32512573+kapilraj7@users.noreply.github.com>
2017-11-21 11:44:12 +0000

    Worked on custom service and its data

--- script.js
+++ script.js
@@ -2,36 +2,48 @@
 var app = angular.module("myApp", ['ngRoute']);
 
 
-var MainController = function($scope) {
+var MainController = function($scope,myService) {
   $scope.message = "This is Home Page";
+  //getting data from different controller using custom service
+  $scope.objData = myService.get();
 };
 
 var AboutController = function($scope) {
-  $scope.message = "This is About Page";
+  $scope.message = "The Customer will fill the contact form in contact page, " + "input all valid details and submit the form. The feedback " + "including all details will be sent to the Home page.";
 };
 
-var ContactController = function($scope) {
+var ContactController = function($scope, myService) {
+  $scope.objArray = [];
   $scope.message = "This is Contact Page";
+  //pushing form data into an array and setting that data in service object 
+  $scope.FormDetails = function() {
+    $scope.objArray.push({
+      name: $scope.name,
+      email: $scope.email,
+      txtarea: $scope.txtarea
+    });
+    myService.set($scope.objArray);
+  };
 };
 
 var RouteApplication = function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
   $routeProvider
   // route for the home page
-  .when('/', {
-      templateUrl : 'home.html',
-      controller : 'mainController'
-  })
-  // route for the about page
-  .when('/about', {
-      templateUrl : 'about.html',
-      controller : 'aboutController'
-  })
-  // route for the contact page
-  .when('/contact', {
-      templateUrl : 'contact.html',
-      controller : 'contactController'
-  });
+    .when('/', {
+      templateUrl: 'home.html',
+      controller: 'mainController'
+    })
+    // route for the about page
+    .when('/about', {
+      templateUrl: 'about.html',
+      controller: 'aboutController'
+    })
+    // route for the contact page
+    .when('/contact', {
+      templateUrl: 'contact.html',
+      controller: 'contactController'
+    });
 };
 
 app.controller("mainController", MainController);
@@ -39,3 +51,20 @@ app.controller("aboutController", AboutController);
 app.controller("contactController", ContactController);
 // configure routes
 app.config(RouteApplication);
+// creating  service
+app.factory('myService', function() {
+  var savedData = {};
+
+  function set(data) {
+    savedData = data;
+  }
+
+  function get() {
+    return savedData;
+  }
+
+  return {
+    set: set,
+    get: get
+  }
+});
